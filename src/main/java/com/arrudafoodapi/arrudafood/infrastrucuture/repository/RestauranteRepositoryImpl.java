fcf7d8c25600a6e4e44c78d01726f648c2600a59
Palc3301 <pedroarruda8@live.com>
2022-12-03 22:24:11 -0300

    End of CriteriaQuery.

--- src/main/java/com/arrudafoodapi/arrudafood/infrastrucuture/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/arrudafoodapi/arrudafood/infrastrucuture/repository/RestauranteRepositoryImpl.java
@@ -1,36 +1,54 @@
 package com.arrudafoodapi.arrudafood.infrastrucuture.repository;
 
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.arrudafoodapi.arrudafood.domain.model.Restaurante;
 import com.arrudafoodapi.arrudafood.repository.RestauranteRepositoryQueries;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
 
 @Repository
 public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
 	@PersistenceContext
 	private EntityManager manager;
-
+	
 	@Override
-	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+	public List<Restaurante> find(String nome, 
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+		var builder = manager.getCriteriaBuilder();
+		
+		var criteria = builder.createQuery(Restaurante.class);
+		var root = criteria.from(Restaurante.class);
 
-		CriteriaBuilder builder = null;
-
-		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-
-		criteria.from(Restaurante.class);
-
-		TypedQuery<Restaurante> query = manager.createQuery(criteria);
+		var predicates = new ArrayList<Predicate>();
+		
+		if (StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		}
+		
+		if (taxaFreteInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+		}
+		
+		if (taxaFreteFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		}
+		
+		criteria.where(predicates.toArray(new Predicate[0]));
+		
+		var query = manager.createQuery(criteria);
+		
 		return query.getResultList();
 	}
+	
 
 }
